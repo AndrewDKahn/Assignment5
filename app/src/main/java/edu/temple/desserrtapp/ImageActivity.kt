c2a6a4b075f2cfeff1f74fce5faab8f430087f41
AndrewDKahn <18akahn.stem@gmail.com>
2021-10-14 06:47:44 -0400

    Finishing up converting string literals to resources

--- app/src/main/java/edu/temple/desserrtapp/ImageActivity.kt
+++ app/src/main/java/edu/temple/desserrtapp/ImageActivity.kt
@@ -31,17 +31,17 @@ class ImageActivity : AppCompatActivity() {
     }
 
     fun generateTestData(stringarray: Array<String>): Array<Item> {
-        return arrayOf(Item(R.drawable.ccf_original, stringarray[2])
-            , Item(R.drawable.ccf_freshstrawberry,stringarray[3])
-            , Item(R.drawable.ccf_chocolatecaramelicious,stringarray[4])
-            , Item(R.drawable.ccf_pineappleupsidedown,stringarray[5])
-            , Item(R.drawable.ccf_celebration,stringarray[6])
-            , Item(R.drawable.ccf_caramelapple,stringarray[7])
-            , Item(R.drawable.ccf_verycherryghirardellichocolate,stringarray[8])
-            , Item(R.drawable.ccf_lowlicious,stringarray[9])
-            , Item(R.drawable.ccf_cinnaboncinnamoncwirl,stringarray[10])
-            , Item(R.drawable.ccf_godiva,stringarray[11])
-            , Item(R.drawable.ccf_coconutcreampie,stringarray[12])
-            , Item(R.drawable.ccf_saltedcaramel,stringarray[13]))
+        return arrayOf(Item(R.drawable.ccf_original, stringarray[0])
+            , Item(R.drawable.ccf_freshstrawberry,stringarray[1])
+            , Item(R.drawable.ccf_chocolatecaramelicious,stringarray[2])
+            , Item(R.drawable.ccf_pineappleupsidedown,stringarray[3])
+            , Item(R.drawable.ccf_celebration,stringarray[4])
+            , Item(R.drawable.ccf_caramelapple,stringarray[5])
+            , Item(R.drawable.ccf_verycherryghirardellichocolate,stringarray[6])
+            , Item(R.drawable.ccf_lowlicious,stringarray[7])
+            , Item(R.drawable.ccf_cinnaboncinnamoncwirl,stringarray[8])
+            , Item(R.drawable.ccf_godiva,stringarray[9])
+            , Item(R.drawable.ccf_coconutcreampie,stringarray[10])
+            , Item(R.drawable.ccf_saltedcaramel,stringarray[11]))
     }
 }
